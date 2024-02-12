889c647ca86e2e362ecbbc229dd3f253827900b1
chenchewei <gunattack1120@gmail.com>
2024-02-12 14:02:05 +0800

    [feat] Finish section 6
    - Put views together and apply navigation into LandmarkList

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,46 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Anderson Chen on 2024/2/12.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree Nation Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.gray)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                
+                Text("Descriptive text goes here")
+            }
+            .padding()
+        }
+        Spacer()
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
