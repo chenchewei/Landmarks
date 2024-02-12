5ca7b9e176f1e0753f2801e6913afdf4e9aa0db7
chenchewei <gunattack1120@gmail.com>
2024-02-12 13:31:20 +0800

    [feat] Finish section 1
    - Add ModelData to manage JSON parse result
    - Separate files in folders

---
+++ Landmarks/Model/ModelData.swift
@@ -0,0 +1,31 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by Anderson Chen on 2024/2/12.
+//
+
+import Foundation
+
+var landmarks: [Landmark] = load("landmarkData.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil) else {
+        fatalError("Unable to find \(filename) in main Bundle.")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Unable to load \(filename) from main Bundle:\n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Failed to parse \(filename) as \(T.self):\n\(error)")
+    }
+}
