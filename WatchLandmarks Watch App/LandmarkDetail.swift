3fc861c0285345ccbcf8a3ad5e0a486c10f05c56
chenchewei <gunattack1120@gmail.com>
2024-02-14 17:03:39 +0800

    [feat] Finish section 3
    - Create the detail view

---
+++ WatchLandmarks Watch App/LandmarkDetail.swift
@@ -0,0 +1,59 @@
+//
+//  LandmarkDetail.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Anderson Chen on 2024/2/14.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    @Environment(ModelData.self) var modelData
+    var landmark: Landmark
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id }) ?? 0
+    }
+    
+    var body: some View {
+        @Bindable var modelData = modelData
+        
+        ScrollView {
+            VStack {
+                CircleImage(image: landmark.image.resizable())
+                    .scaledToFit()
+
+                Text(modelData.landmarks[landmarkIndex].name)
+                    .font(.headline)
+                    .lineLimit(0)
+                
+                Toggle(isOn: $modelData.landmarks[landmarkIndex].isFavorite, label: {
+                    Text("Favorite")
+                })
+                
+                Divider()
+                
+                Text(landmark.park)
+                    .font(.caption)
+                    .bold()
+                    .lineLimit(0)
+                
+                Text(landmark.state)
+                    .font(.caption)
+                
+                Divider()
+                
+                MapView(coordinate: landmark.locationCoordinates)
+                    .scaledToFit()
+            }
+            .padding(16)
+        }
+        .navigationTitle("Landmarks")
+        
+    }
+}
+
+#Preview {
+    let modelData = ModelData()
+    return LandmarkDetail(landmark: modelData.landmarks[0])
+        .environment(modelData)
+}
