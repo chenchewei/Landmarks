5c656b1284608c1c97c4688155564c36d0bd7b57
chenchewei <gunattack1120@gmail.com>
2024-02-14 15:45:32 +0800

    [feat] Finish section 3
    - Track the page in a SwiftUI view’s state using PageViewControllerDelegate

--- Landmarks/Views/PageView/PageViewController.swift
+++ Landmarks/Views/PageView/PageViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
     var pages: [Page]
+    @Binding var currentPage: Int
     
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
@@ -21,18 +22,19 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
             navigationOrientation: .horizontal
         )
         pageVC.dataSource = context.coordinator
+        pageVC.delegate = context.coordinator
         return pageVC
     }
     
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
         pageViewController.setViewControllers(
-            [context.coordinator.controllers[0]],
+            [context.coordinator.controllers[currentPage]],
             direction: .forward,
             animated: true
         )
     }
     
-    class Coordinator: NSObject, UIPageViewControllerDataSource {
+    class Coordinator: NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
         var parent: PageViewController
         var controllers: [UIViewController] = []
         
@@ -55,6 +57,12 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
             }
             
             return index + 1 == controllers.count ? controllers.first : controllers[index + 1]
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+            if completed, let visibleVC = pageViewController.viewControllers?.first, let index = controllers.firstIndex(of: visibleVC) {
+                parent.currentPage = index
+            }
         }
     }
     
