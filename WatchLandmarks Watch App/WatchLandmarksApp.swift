5ff595b13ee6293502db65963cc85bf67ef613c9
chenchewei <gunattack1120@gmail.com>
2024-02-14 16:41:25 +0800

    [chore] Finish section 1
    - Add a watchOS target

---
+++ WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Anderson Chen on 2024/2/14.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
